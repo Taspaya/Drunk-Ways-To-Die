71500a78d600fe28c08134daa6d922e24ad47dfb
Taspaya <taspayaa@gmail.com>
2017-07-21 17:00:11 +0200

    Add Drunk
    Boton para añadir borrachos completado

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    List<string> borrachos = new List<string> ();
+
+    public GameObject InputField;
+    public Text PlayerName;
+    public string name;
+
+
+
+   
+    void Start () {
+        InputField.SetActive(false);                         // El cuadro de texto empieza en falso
+    }
 	
-	// Update is called once per frame
+
 	void Update () {
-		
 	}
+
+    public void addPlayer() {
+        InputField.SetActive(true);                         // quando pulsamos el boton, se activa el cuadro de texto
+        PlayerName.text = " ";                               // intentamos resetear el cuadro de texto
+    }
+    public void RealAddPlayer() {
+
+        name = PlayerName.text.ToString();                  // guardamos el texto en la variable name
+        borrachos.Add(name);                                // insertamos el name en la List
+       // Debug.Log(borrachos[0]);
+        Debug.Log("Afegit: " + name);                       // comprovamos que se ha añadido
+        PlayerName.text = " ";                               // intentamos resetear el cuadro de texto
+        InputField.SetActive(false);                        // desactivamos el cuadro de texto
+        
+    }
 }
