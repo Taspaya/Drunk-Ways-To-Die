634e0b8f2028815a5a83067c65f759dc8811c2ca
Taspaya <taspayaa@gmail.com>
2017-07-22 17:28:47 +0200

    Modo smartphone
    Activado

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,9 @@ public class MainMenu : MonoBehaviour
 
     List<string> borrachos = new List<string>();
 
-    public GameObject InputField;
+    public GameObject InputFieldCanvas;
+    public GameObject BorrachosNamesCanvas;
+    public GameObject SettingsCanvas;
     public Text PlayerName;
     public Text PlayersNames;
     public string name;
@@ -18,14 +20,16 @@ public class MainMenu : MonoBehaviour
 
     void Start()
     {
-        InputField.SetActive(false);                         // El cuadro de texto empieza en falso
+        InputFieldCanvas.SetActive(false);                         // El cuadro de texto empieza en falso
+        BorrachosNamesCanvas.SetActive(false);
+        SettingsCanvas.SetActive(false);
 
-       /* borrachos.Add("Gerard");
-        borrachos.Add("Javi");
-        borrachos.Add("Oscar");
-        borrachos.Add("Radev");
-        borrachos.Add("Tona");
-        borrachos.Add("El Jaume");*/
+         borrachos.Add("Gerard");
+         borrachos.Add("Javi");
+         borrachos.Add("Oscar");
+         borrachos.Add("Radev");
+         borrachos.Add("Tona");
+         borrachos.Add("El Jaume");
     }
 
 
@@ -35,31 +39,48 @@ public class MainMenu : MonoBehaviour
 
     public void addPlayer()
     {
-        InputField.SetActive(true);                         // quando pulsamos el boton, se activa el cuadro de texto
+        InputFieldCanvas.SetActive(true);                         // quando pulsamos el boton, se activa el cuadro de texto
         PlayerName.text = null;                               // intentamos resetear el cuadro de texto
     }
     public void RealAddPlayer()
     {
 
         name = PlayerName.text.ToString();                  // guardamos el texto en la variable name
-        borrachos.Add(name);                                // insertamos el name en la List
-                                                            // Debug.Log(borrachos[0]);
-        Debug.Log("Afegit: " + name);                       // comprovamos que se ha añadido
-        PlayerName.text = " ";                              // intentamos resetear el cuadro de texto
-        InputField.SetActive(false);                        // desactivamos el cuadro de texto
 
+        if (name != "")
+        {
+            borrachos.Add(name);                                // insertamos el name en la List
+                                                                // Debug.Log(borrachos[0]);
+            Debug.Log("Afegit: " + name);                       // comprovamos que se ha añadido
+            PlayerName.text = " ";                              // intentamos resetear el cuadro de texto
+            InputFieldCanvas.SetActive(false);                  // desactivamos el cuadro de texto
+        }
     }
 
+    public void BorrachosBack() {
+        BorrachosNamesCanvas.SetActive(false);
+    }
+    public void OptionsBTN()
+    {
+        SettingsCanvas.SetActive(true);
+    }
+    public void OptionsBackBTN() {
+        SettingsCanvas.SetActive(false);
+    }
+    public void BorrarBorrachos()
+    {
+       // borrachos = null;
+       borrachos = new List<string>();
+    }
     public void namesWriter()
     {
+        BorrachosNamesCanvas.SetActive(true);
 
         PlayersNames.text = null;                           // reseteas el texto, para que no se acumule
         for (int i = 0; i < borrachos.Count; i++)           // recorres el size de la list
         {
             name = PlayersNames.text.ToString();            // acumulas lo que havia en el texto
-            PlayersNames.text = name + "\n\n" + borrachos[i];// Añades el siguiente
-            
-            
+            PlayersNames.text = name + "\n\n" + borrachos[i];// Añades el siguiente           
         }
     }
 }
